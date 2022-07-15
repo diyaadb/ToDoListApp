19968446e90d7dbf442c73573833cac2d89340ed
Diya Bhatia <diyabhatia1@gmail.com>
2022-07-15 15:41:13 -0400

    Iteration 0

---
+++ ToDoListApp/TableViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TableViewController.swift
+//  ToDoListApp
+//
+//  Created by Scholar on 7/15/22.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    var toDos : [ToDo] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let toDo = toDos[indexPath.row]
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func createToDos() -> [ToDo] {
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+
